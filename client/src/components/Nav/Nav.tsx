cbd34888a4533b7684ee4875b4f095bda2b02fd9
Ollie-Grayson <oliver.grayson@noricangroup.com>
2025-04-22 14:56:09 +0100

    Update Nav.tsx
    fixing back to original after failed DeepL int

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -1,84 +1,111 @@
-import { RecoilRoot } from 'recoil';
-import { DndProvider } from 'react-dnd';
-import { RouterProvider } from 'react-router-dom';
-import * as RadixToast from '@radix-ui/react-toast';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { QueryClient, QueryClientProvider, QueryCache } from '@tanstack/react-query';
-import { ScreenshotProvider, ThemeProvider, useApiErrorBoundary } from './hooks';
-import { ToastProvider } from './Providers';
-import Toast from './components/ui/Toast';
-import { LiveAnnouncer } from '~/a11y';
-import { router } from './routes';
+import React, { useEffect } from 'react';
+import ReactMarkdown from 'react-markdown';
+import TagManager from 'react-gtm-module';
+import { Constants } from 'librechat-data-provider';
+import { useGetStartupConfig } from '~/data-provider';
+import { useLocalize } from '~/hooks';
 
-const App = () => {
-  const { setError } = useApiErrorBoundary();
+export default function Footer({ className }: { className?: string }) {
+  const { data: config } = useGetStartupConfig();
+  const localize = useLocalize();
 
-  const queryClient = new QueryClient({
-    queryCache: new QueryCache({
-      onError: (error) => {
-        if (error?.response?.status === 401) {
-          setError(error);
-        }
-      },
-    }),
-  });
+  const privacyPolicy = config?.interface?.privacyPolicy;
+  const termsOfService = config?.interface?.termsOfService;
 
-  const handleGoToDeepL = () => {
-    window.location.href = '/deepl-translator'; // Navigate to the new page
-  };
+  const privacyPolicyRender = privacyPolicy?.externalUrl != null && (
+    <a
+      className="text-text-secondary underline"
+      href={privacyPolicy.externalUrl}
+      target={privacyPolicy.openNewTab === true ? '_blank' : undefined}
+      rel="noreferrer"
+    >
+      {localize('com_ui_privacy_policy')}
+    </a>
+  );
+
+  const termsOfServiceRender = termsOfService?.externalUrl != null && (
+    <a
+      className="text-text-secondary underline"
+      href={termsOfService.externalUrl}
+      target={termsOfService.openNewTab === true ? '_blank' : undefined}
+      rel="noreferrer"
+    >
+      {localize('com_ui_terms_of_service')}
+    </a>
+  );
+
+  const mainContentParts = (
+    typeof config?.customFooter === 'string'
+      ? config.customFooter
+      : '[LibreChat ' +
+        Constants.VERSION +
+        '](https://librechat.ai) - ' +
+        localize('com_ui_latest_footer')
+  ).split('|');
+
+  useEffect(() => {
+    if (config?.analyticsGtmId != null && typeof window.google_tag_manager === 'undefined') {
+      const tagManagerArgs = {
+        gtmId: config.analyticsGtmId,
+      };
+      TagManager.initialize(tagManagerArgs);
+    }
+  }, [config?.analyticsGtmId]);
+
+  const mainContentRender = mainContentParts.map((text, index) => (
+    <React.Fragment key={`main-content-part-${index}`}>
+      <ReactMarkdown
+        components={{
+          a: ({ node: _n, href, children, ...otherProps }) => {
+            return (
+              <a
+                className="text-text-secondary underline"
+                href={href}
+                target="_blank"
+                rel="noreferrer"
+                {...otherProps}
+              >
+                {children}
+              </a>
+            );
+          },
+
+          p: ({ node: _n, ...props }) => <span {...props} />,
+        }}
+      >
+        {text.trim()}
+      </ReactMarkdown>
+    </React.Fragment>
+  ));
+
+  const footerElements = [...mainContentRender, privacyPolicyRender, termsOfServiceRender].filter(
+    Boolean,
+  );
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <LiveAnnouncer>
-          <ThemeProvider>
-            <RadixToast.Provider>
-              <ToastProvider>
-                <DndProvider backend={HTML5Backend}>
-                  <RouterProvider router={router} />
-                  <ReactQueryDevtools initialIsOpen={false} position="top-right" />
-                  <Toast />
-                  <RadixToast.Viewport className="pointer-events-none fixed inset-0 z-[1000] mx-auto my-2 flex max-w-[560px] flex-col items-stretch justify-start md:pb-5" />
-                  <button
-                    type="button"
-                    onClick={handleGoToDeepL}
-                    style={{
-                      position: 'fixed',
-                      top: '10px',
-                      right: '10px',
-                      padding: '10px 20px',
-                      backgroundColor: '#007bff',
-                      color: '#fff',
-                      border: 'none',
-                      borderRadius: '5px',
-                      cursor: 'pointer',
-                      zIndex: 1000,
-                    }}
-                  >
-                    Go To DeepL Document Translator
-                  </button>
-                </DndProvider>
-              </ToastProvider>
-            </RadixToast.Provider>
-          </ThemeProvider>
-        </LiveAnnouncer>
-      </RecoilRoot>
-    </QueryClientProvider>
+    <div className="relative w-full">
+      <div
+        className={
+          className ??
+          'absolute bottom-0 left-0 right-0 hidden items-center justify-center gap-2 px-2 py-2 text-center text-xs text-text-primary sm:flex md:px-[60px]'
+        }
+        role="contentinfo"
+      >
+        {footerElements.map((contentRender, index) => {
+          const isLastElement = index === footerElements.length - 1;
+          return (
+            <React.Fragment key={`footer-element-${index}`}>
+              {contentRender}
+              {!isLastElement && (
+                <div
+                  key={`separator-${index}`}
+                  className="h-2 border-r-[1px] border-border-medium"
+                />
+              )}
+            </React.Fragment>
+          );
+        })}
+      </div>
+    </div>
   );
-};
-
-export default () => (
-  <ScreenshotProvider>
-    <App />
-    <iframe
-      src="/assets/silence.mp3"
-      allow="autoplay"
-      id="audio"
-      title="audio-silence"
-      style={{
-        display: 'none',
-      }}
-    />
-  </ScreenshotProvider>
-);
+}
